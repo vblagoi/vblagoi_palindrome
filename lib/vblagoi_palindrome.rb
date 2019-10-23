dd69e624cc8706987905b2a94e5349e2453e33a2
volodymyr <vblagoi@gmail.com>
2019-10-23 20:09:09 +0300

    Finish working and refactored palindrome method

--- lib/vblagoi_palindrome.rb
+++ lib/vblagoi_palindrome.rb
@@ -1,6 +1,16 @@
 require "vblagoi_palindrome/version"
 
-module VblagoiPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
